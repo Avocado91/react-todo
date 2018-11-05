41e8be1ae7974e60c39c808f5c806c1f2f68e0c4
T.J <t.ruggles@live.ca>
2018-11-04 23:56:31 -0500

    Basic component structure completed

--- src/index.js
+++ src/index.js
@@ -4,10 +4,55 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   render() {
     return (
-      <div>div</div>
-    )
+      <div>
+      <Header></Header>
+      <TodoList></TodoList>
+      <NewTodo></NewTodo>
+      <AddTodo></AddTodo>
+      </div>
+    );
   }
-}
+};
+
+const Header = () => (
+  <div>
+    <img src='./../images/react.png' alt='React Logo'></img>
+    <h1>React To Do</h1>
+  </div>
+);
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>item 1 </li>
+          <li>item 2</li>
+          <li>item 3</li>
+        </ul>
+      </div>
+    );
+  }
+};
+
+const NewTodo = () => (
+  <form>
+    <input type='text'></input>
+  </form>
+);
+      
+    
+
+
+class AddTodo extends React.Component {
+  render() {
+    return (
+      <button>+</button>
+    );
+  }
+};
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
