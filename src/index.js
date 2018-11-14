1e55ffb86a4fb6287648f18847e1de0ae81ccf1a
T.J <t.ruggles@live.ca>
2018-11-14 00:01:33 -0500

    Added node-sass to dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './../src/components/App';
+import './styles/styles.scss';
 
 ReactDOM.render(<App></App>, document.getElementById('root'));
